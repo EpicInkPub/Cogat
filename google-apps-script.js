bf8d44aaefa7c0eefaac99bdac7dd453657b63fd
EpicInkPub <epicinkpublishing@gmail.com>
2025-10-06 23:08:23 +0200

    Publish application

--- google-apps-script.js
+++ google-apps-script.js
@@ -1,5 +1,5 @@
 // Replace with your actual Google Sheet ID
-const SPREADSHEET_ID = "YOUR_SPREADSHEET_ID_HERE";
+const SPREADSHEET_ID = "1spg_4-vWPKdTZOUFNLTPibxvA-BYcrQaPLDIkbe37bM";
 const SHEET_NAME = "Raw Data";
 
 function doPost(e) {
